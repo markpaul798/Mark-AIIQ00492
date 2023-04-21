6deb8e2cb542beb7d553e10994b073bfc10460dc
markpaul798 <131008934+markpaul798@users.noreply.github.com>
2023-04-21 18:57:44 +0300

    IBL

---
+++ deep.py
@@ -0,0 +1,6 @@
+Answer = input(“What is the answer to the Great Question of Life, the Universe and Everything? “)
+
+If answer.lower() in [“42”, “forty-two”, “forty two”]:
+    Print(“Yes”)
+Else:
+    Print(“No”)
